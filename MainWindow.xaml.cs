052ae7556567dadaa98ca6c62a5254d7ade844be
RyanP <ryanp@javs.com>
2022-10-24 13:23:22 -0400

    WIP Initial Code and Layout

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace StageTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer WorkerTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Background = Brushes.Black;
+
+            WorkerTimer = new DispatcherTimer();
+            WorkerTimer.Interval = new TimeSpan(0, 0, 1);
+            WorkerTimer.Tick += WorkerTimer_Tick;
+            WorkerTimer.Start();
+        }
+
+        private void WorkerTimer_Tick(object sender, EventArgs e)
+        {
+            TimeOfDay.Content = DateTime.Now.ToString();
+        }
+    }
+}
---
+++ TimeSlotEventList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StageTimer
+{
+   [Serializable]
+    class TimeSlotEventList
+    {
+        
+    }
+
+    [Serializable]
+    class TimeSlotEvent
+    {
+        DateTime StartTime;
+        DateTime EndTime;
+        String Name;
+        int WarningTime;
+    }
+}
